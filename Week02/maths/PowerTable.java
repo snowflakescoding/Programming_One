5948f9aab636523cde9816367b333dbf7b442d9a
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 09:15:33 +0700

    Update and rename PowTable.java to PowerTable.java

---
+++ Week02/maths/PowerTable.java
@@ -0,0 +1,22 @@
+public class PowerTable {
+    public static void main(String[] args) {
+        System.out.println("a      b      pow(a , b)");
+        int a1 = 1, b1 = 2;
+        int power1 = (int) Math.pow(a1, b1);
+        System.out.println(a1 + "      " + b1 + "           " + power1);
+        int a2 = 2, b2 = 3;
+        int power2 = (int) Math.pow(a2, b2);
+        System.out.println(a2 + "      " + b2 + "           " + power2);
+        int a3 = 3, b3 = 4;
+        int power3 = (int) Math.pow(a3, b3);
+        System.out.println(a3 + "      " + b3 + "          " + power3);
+        int a4 = 4, b4 = 5;
+        int power4 = (int) Math.pow(a4, b4);
+        System.out.println(a4 + "      " + b4 + "        " + power4);
+        int a5 = 5, b5 = 6;
+        int power5 = (int) Math.pow(a5, b5);
+        System.out.println(a5 + "      " + b5 + "       " + power5);
+    }
+}
+
+

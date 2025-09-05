7659ed2a960d51dcd7b4e50181914384bcf3b0b7
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 08:44:32 +0700

    Update Activity6.java

--- Week01/Activity6.java
+++ Week01/Activity6.java
@@ -1,7 +1,5 @@
-package Practice.Exercises.Week01;
-
 public class Activity6 {
-    public static void main(String[]args){
-        System.out.println("A = " + ((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5)));
-    }
+  public static void main(String[] args) {
+    System.out.println((9.5 * 4.5 - 2.5 * 3) / (45.5 - 3.5));
+  }
 }

761bd03a2b67c244c2baab60cd7f7c8e7ceb3411
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 08:45:09 +0700

    Update Activity7.java

--- Week01/Activity7.java
+++ Week01/Activity7.java
@@ -1,16 +1,15 @@
-package Practice.Exercises.Week01;
-
 public class Activity7 {
-	public static void main(String[] args) {
-		System.out.println("Population after 1 year: "
-				+ (312032486 + 365 * 24 * 60 * 60 / 7 - 365 * 24 * 60 * 60 / 13 + 365 * 24 * 60 * 60 / 45));
-		System.out.println("Population after 2 years: "
-				+ (312032486 + 365 * 2 * 24 * 60 * 60 / 7 - 365 * 2 * 24 * 60 * 60 / 13 + 365 * 2 * 24 * 60 * 60 / 45));
-		System.out.println("Population after 3 years: "
-				+ (312032486 + 365 * 3 * 24 * 60 * 60 / 7 - 365 * 3 * 24 * 60 * 60 / 13 + 365 * 3 * 24 * 60 * 60 / 45));
-		System.out.println("Population after 4 years: "
-				+ (312032486 + 365 * 4 * 24 * 60 * 60 / 7 - 365 * 4 * 24 * 60 * 60 / 13 + 365 * 4 * 24 * 60 * 60 / 45));
-		System.out.println("Population after 5 years: "
-				+ (312032486 + 365 * 5 * 24 * 60 * 60 / 7 - 365 * 5 * 24 * 60 * 60 / 13 + 365 * 5 * 24 * 60 * 60 / 45));
-	}
+    public static void main(String[] args) {
+        System.out.println("Population projection for the next five years:");
+
+        System.out.println("Year 1 Population: " + (int) (312032486.0 + ((365.0 * 24 * 60 * 60) / 7.0) - ((365.0 * 24 * 60 * 60) / 13.0) + ((365.0 * 24 * 60 * 60) / 45.0)));
+
+        System.out.println("Year 2 Population: " + (int) (312032486.0 + (((365.0 * 24 * 60 * 60) / 7.0) - ((365.0 * 24 * 60 * 60) / 13.0) + ((365.0 * 24 * 60 * 60) / 45.0)) * 2));
+
+        System.out.println("Year 3 Population: " + (int) (312032486.0 + (((365.0 * 24 * 60 * 60) / 7.0) - ((365.0 * 24 * 60 * 60) / 13.0) + ((365.0 * 24 * 60 * 60) / 45.0)) * 3));
+
+        System.out.println("Year 4 Population: " + (int) (312032486.0 + (((365.0 * 24 * 60 * 60) / 7.0) - ((365.0 * 24 * 60 * 60) / 13.0) + ((365.0 * 24 * 60 * 60) / 45.0)) * 4));
+
+        System.out.println("Year 5 Population: " + (int) (312032486.0 + (((365.0 * 24 * 60 * 60) / 7.0) - ((365.0 * 24 * 60 * 60) / 13.0) + ((365.0 * 24 * 60 * 60) / 45.0)) * 5));
+    }
 }

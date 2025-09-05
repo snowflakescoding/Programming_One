b81ef3822175a2671b3a3debd91430db7f36a2a8
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 09:54:07 +0700

    Update FinancialCompoundValue.java

--- Week02/financial/FinancialCompoundValue.java
+++ Week02/financial/FinancialCompoundValue.java
@@ -1,22 +1,23 @@
-package Practice.Exercises.Week02.financial;
-
 import java.util.Scanner;
 
-public class FinancialCompoundValue {
+public class FinancialValue {
     public static void main(String[] args) {
         try (Scanner scanner = new Scanner(System.in)) {
-            System.out.print("Enter the monthly saving amount (in VND): ");
+            System.out.println("Enter the monthly saving amount:  ");
+            
+            // declare monthly savings and interest rate
             double monthlySaving = scanner.nextDouble();
             double monthlyInterestRate = 0.05 / 12;
-            double accountValue = 0;
+            
+            // declare account value, starting from 0
+            double accountValue = 0.0;
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 1
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 2
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 3
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 4
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 5
             accountValue = (accountValue + monthlySaving) * (1 + monthlyInterestRate); // Month 6
-            System.out.println("The account value after 6 months is: " + accountValue);
+            System.out.printf("After the sixth month, the account value is $%.2f%n", accountValue);
         }
     }
 }
-

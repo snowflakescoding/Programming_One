e8e0d4845e6ec873d3506c2ed23f6ad9e5f47813
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 08:50:48 +0700

    Update Activity8.java

--- Week01/Activity8.java
+++ Week01/Activity8.java
@@ -1,8 +1,6 @@
-package Practice.Exercises.Week01;
-
 public class Activity8 {
     public static void main(String[] args) {
-        System.out.println("x = " + ((44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1)));
-        System.out.println("y = " + ((3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1)));
+        System.out.println("x = " + (44.5 * 0.55 - 50.2 * 5.9) / (3.4 * 0.55 - 50.2 * 2.1)); // print result of x using cramer's rule
+        System.out.println("y = " + (3.4 * 5.9 - 44.5 * 2.1) / (3.4 * 0.55 - 50.2 * 2.1)); // print result of y using cramer's rule
     }
 }

2dd5f94e780824de58e4a563afb0a86e0da12ba8
snowflakescoding <ducnghiepdao@gmail.com>
2025-09-05 08:43:49 +0700

    Update Activity5.java

--- Week01/Activity5.java
+++ Week01/Activity5.java
@@ -1,6 +1,6 @@
 public class Activity5 {
-    public static void main(String[]args){
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // task: "print Hello World!"
     }
-
 }
+
